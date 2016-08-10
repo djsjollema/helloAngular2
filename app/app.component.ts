dcbb23b068978949534d090f0017d31e7b65ef7e
Jelle Sjollema <djsjollema@gmail.com>
2016-08-10 22:11:30 +0200

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'hello-world',
+	template: '<h1>Hello World - dit is Angular 2!</h1><h2>Dit is de eerste applicatie</h2>'
+})
+export class AppComponent {
+	// ... hier komt de logica
+}
